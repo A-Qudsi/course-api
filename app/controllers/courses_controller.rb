56c06b8437d99204998b6fa5e9b4bb0f686d1ee3
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-12-06 18:40:03 -0500

    courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,51 @@
 class CoursesController < ApplicationController
+
+     def index 
+        @courses = Course.all 
+        render json: @courses
+    end
+
+    def show
+        @course = Course.find(params[:id])
+        render json: @course 
+    end
+
+    def create
+        @course = Course.new(course_params)
+
+        if @course.save 
+            render json: @course 
+        else
+            render json: @course.errors.full_messages, status: 422
+        end
+
+    end
+
+    def update 
+        @course = Course.find(params[:id])
+
+        if @course.update(course_params)
+            render json: @course
+        else
+            render json: @course.errors.full_messages, status: 422
+        end
+
+    end
+
+    def destroy
+        @course = Course.find_by(id: params[:id])
+
+        if @course && @course.destroy 
+            render json: @course 
+        else 
+            render json: @course.errors.full_messages, status: 422
+        end
+
+    end
+
+    private
+
+    def course_params
+        params.require(:course).permit(:name, :release_date)
+    end
 end
