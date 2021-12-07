01a8d875d573970cf6d449dea02336d2a56beb62
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-12-07 01:21:40 -0500

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+ActiveRecord::Base.transaction do
+Course.destroy_all
+
+history = Course.create(name: "History", release_date: "2021-12-06")
+math = Course.create(name: "Math", release_date: "2021-12-06")
+ruby = Course.create(name: "Ruby", release_date: "1995-01-01")
+end
